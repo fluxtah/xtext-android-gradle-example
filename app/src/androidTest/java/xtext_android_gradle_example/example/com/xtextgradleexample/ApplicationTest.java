1876dd357517c55e0ec0a0d652a2393cb21f9176
Ian Warwick <ian.warwick@just-eat.com>
2015-03-31 13:25:25 +0100

    android xtext gradle example

---
+++ app/src/androidTest/java/xtext_android_gradle_example/example/com/xtextgradleexample/ApplicationTest.java
@@ -0,0 +1,13 @@
+package xtext_android_gradle_example.example.com.xtextgradleexample;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
